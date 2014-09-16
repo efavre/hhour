1da6f6e34c438cd0cfea5ac0acd236ee7d180296
Eric Favre <ericfavreddd@gmail.com>
2014-09-16 18:18:28 +0200

    add devices to activeadmin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 
 	def create
-		user = User.find(:first_name => params[:user][:first_name])
-		Device.create(:token => params[:user][:token], :user => user)
+		user = User.find_by(first_name: params[:user][:first_name])
+		if user
+			Device.create(:token => params[:user][:token], :user => user)
+		end
+		render nothing: true
 	end
 
 end
