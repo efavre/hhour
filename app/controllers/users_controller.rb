d4fd7dabc83ccb097875f6ee0d15ec97725bcb9d
Eric Favre <ericfavreddd@gmail.com>
2014-09-17 10:19:12 +0200

    register device token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
+	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
 	def create
 		user = User.find_by(first_name: params[:user][:first_name])
 		if user
 			Device.create(:token => params[:user][:token], :user => user)
 		end
-		render nothing: true
+		render action: :index
 	end
 
 end
