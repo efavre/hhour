9f3a6ed3078bfe4dd124059351f9cb2bd80522db
Eric Favre <ericfavreddd@gmail.com>
2014-09-17 18:05:35 +0200

    notify when challenge taken

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,21 @@
 class Picture < ActiveRecord::Base
 	belongs_to :author, class_name: "User"
 	belongs_to :picture_thread
+	after_create :notify
+
+	def notify
+		notifications = []
+		Device.all.each do |device|
+			notification = Houston::Notification.new(device: device.token)
+			notification.alert = "Une nouvelle réponse au challenge #{self.picture_thread.title}"
+			notification.badge = 1
+			notifications << notification
+		end
+		APN.push(notifications)
+	end
 
 	def get_as3_url
 		S3_BUCKET.objects[file_key].url_for(:read).to_s
 	end
+
 end
