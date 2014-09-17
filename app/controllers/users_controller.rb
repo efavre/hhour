d67d0879f4e03d0e520bd96b312788ca1e63ff64
Eric Favre <ericfavreddd@gmail.com>
2014-09-17 16:33:31 +0200

    find or create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
 	def create
-		user = User.find_or_create(first_name: params[:user][:first_name])
+		user = User.find_or_create_by(first_name: params[:user][:first_name])
 		if user
 			Device.create(:token => params[:user][:token], :user => user)
 		end
