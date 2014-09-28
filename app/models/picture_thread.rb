caae54af94d738d0313e37394a92757290b1437f
Eric Favre <ericfavreddd@gmail.com>
2014-09-28 12:49:59 +0200

    save closing date

--- app/models/picture_thread.rb
+++ app/models/picture_thread.rb
@@ -6,7 +6,7 @@ class PictureThread < ActiveRecord::Base
 	after_create :notify, :set_closing_date
 
 	def set_closing_date
-		self.update_attribute(:closing_date, self.created_at + 1.hour}
+		self.update_attribute(:closing_date, self.created_at + 1.hour)
 	end
 
 	def notify
