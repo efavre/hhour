d30f06b23e899d7ea7d1c26c87cf95c7dd5940e5
Eric Favre <ericfavreddd@gmail.com>
2014-10-22 21:47:19 +0200

    no validation on comment title

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
   default_scope -> { order('created_at ASC') }
-  validates :title, presence: true
   validates :comment, presence: true
   validates :user, presence: true
 
